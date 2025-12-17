b774d05753bf373b02e1c7c6815addbad0650f85
Shay Goldstein <shay_goldstein@mckinsey.com>
2025-12-17 15:28:44 +0200

    Add Rocky 8 compatibility patches, /etc/hosts fix, and calico OOM fix

--- igz_files/igz_deploy.sh
+++ igz_files/igz_deploy.sh
@@ -18,28 +18,28 @@ DEPLOYMENT_PLAYBOOK="cluster.yml"
 # Get the deploy options
 for arg in "$@"
 do
-   if [ "$arg" == "--reset" ]; then
-       echo "Reset was requested"
-       RESET="yes"
-   elif [ "$arg" == "--skip-k8s-install" ]; then
-       echo "skip-k8s-install was requested"
-       SKIP_INSTALL="yes"
-   elif [ "$arg" == "--scale" ]; then
-       echo "Scale out requested"
-       SCALE_OUT="yes"
-       DEPLOYMENT_PLAYBOOK="scale.yml"
-   elif [ "$arg" == "--renew" ]; then
-       echo "Certificate renewal was requested"
-       RENEW="yes"
-       RESET="no"
-       SKIP_INSTALL="yes"
-   fi
+    if [ "$arg" == "--reset" ]; then
+        echo "Reset was requested"
+        RESET="yes"
+        elif [ "$arg" == "--skip-k8s-install" ]; then
+        echo "skip-k8s-install was requested"
+        SKIP_INSTALL="yes"
+        elif [ "$arg" == "--scale" ]; then
+        echo "Scale out requested"
+        SCALE_OUT="yes"
+        DEPLOYMENT_PLAYBOOK="scale.yml"
+        elif [ "$arg" == "--renew" ]; then
+        echo "Certificate renewal was requested"
+        RENEW="yes"
+        RESET="no"
+        SKIP_INSTALL="yes"
+    fi
 done
 
 # Verify deploy options
 if [ "$SCALE_OUT" == "yes" ]; then
- RESET="no"
- SKIP_INSTALL="no"
+    RESET="no"
+    SKIP_INSTALL="no"
 fi
 
 echo "==> Build Iguazio inventory"
@@ -47,9 +47,9 @@ python3 ./igz_inventory_builder.py "${@: -4}"
 
 # Don't stop containers in case of scale out - it's a live data node!
 if [ "$SCALE_OUT" == "yes" ]; then
-  echo -e "\n# Live system flag\nlive_system: true" >> igz_override.yml
+    echo -e "\n# Live system flag\nlive_system: true" >> igz_override.yml
 else
-  echo -e "\n# Live system flag\nlive_system: false" >> igz_override.yml
+    echo -e "\n# Live system flag\nlive_system: false" >> igz_override.yml
 fi
 
 echo "==> Prepare inventory dir"
@@ -74,16 +74,21 @@ if [[ "${RENEW}" == "yes" ]]; then
 fi
 
 # Run igz_preinstall playbook
-./igz_run_ansible.sh -i inventory/igz/igz_inventory.ini igz_pre_install.yml --become --extra-vars=@igz_override.yml
+# Skip /etc/hosts update when not installing k8s (already configured from previous install)
+if [[ "${SKIP_INSTALL}" == "yes" ]]; then
+    ./igz_run_ansible.sh -i inventory/igz/igz_inventory.ini igz_pre_install.yml --become --extra-vars=@igz_override.yml --extra-vars skip_hosts_update=true
+else
+    ./igz_run_ansible.sh -i inventory/igz/igz_inventory.ini igz_pre_install.yml --become --extra-vars=@igz_override.yml
+fi
 
 # Run offline repo playbook
 ./igz_run_ansible.sh -i inventory/igz/igz_inventory.ini playbook/offline-repo.yml --become --extra-vars=@igz_override.yml
 
 # Reset Kubespray
 if [[ "${RESET}" == "yes" ]]; then
-  echo "==> Reset Kubernetes"
-  ./igz_run_ansible.sh -i inventory/igz/igz_inventory.ini reset.yml --become --extra-vars=@igz_override.yml --extra-vars reset_confirmation=yes
-  ./igz_run_ansible.sh -i inventory/igz/igz_inventory.ini igz_reset.yml --become --extra-vars=@igz_override.yml
+    echo "==> Reset Kubernetes"
+    ./igz_run_ansible.sh -i inventory/igz/igz_inventory.ini reset.yml --become --extra-vars=@igz_override.yml --extra-vars reset_confirmation=yes
+    ./igz_run_ansible.sh -i inventory/igz/igz_inventory.ini igz_reset.yml --become --extra-vars=@igz_override.yml
 fi
 
 # Run kubespray
